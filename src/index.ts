04e0c1a972767f60c2f00489417e50d550dfd9ea
rikarani <46773591+rikarani@users.noreply.github.com>
2024-09-19 23:42:04 +0700

    Refactor index.ts to use Genshin and MLBB plugins

--- src/index.ts
+++ src/index.ts
@@ -1,46 +1,7 @@
 import { Elysia } from "elysia";
 
-import { mlbbModel } from "@/models/mlbb";
-import { mlbb } from "@/handlers/mlbb";
+import { mlbb, genshin } from "./plugins";
 
-const app = new Elysia()
-  .use(mlbbModel)
-  .get("/mlbb", ({ query: { id, zone } }) => mlbb({ id, zone }), {
-    query: "mlbb.query",
-    response: "mlbb.response",
-    error({ error, code, set }) {
-      set.headers["content-type"] = "application/json";
-
-      if (code === "VALIDATION") {
-        return {
-          success: false,
-          errors: error.all
-            .filter((x) => {
-              return "type" in x && x.type === 54;
-            })
-            .map((x) => {
-              return {
-                path: "path" in x && x.path,
-                message: "message" in x && x.message,
-                summary: x.summary,
-              };
-            }),
-        };
-      }
-
-      if (code === "UNKNOWN") {
-        set.status = "Not Found";
-
-        return {
-          success: false,
-          error: {
-            name: error.name,
-            message: error.message,
-          },
-        };
-      }
-    },
-  })
-  .listen(3000);
+const app = new Elysia().use(mlbb).use(genshin).listen(3000);
 
 console.log(`🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`);
