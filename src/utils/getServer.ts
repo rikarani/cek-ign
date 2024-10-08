a271da1efcea350a6ee50a3f77479804ccf9074f
rikarani <46773591+rikarani@users.noreply.github.com>
2024-10-08 15:55:20 +0700

    Refactor getServer.ts to use Server enum instead of Genshin enum

--- src/utils/getServer.ts
+++ src/utils/getServer.ts
@@ -1,25 +1,25 @@
-import { Genshin } from "@/types/Server";
+import { Genshin as Server } from "@/types/Server";
 import { InvalidUID } from "@/errors/InvalidUID";
 
 type Params = {
   uid: string;
 };
 
-export function getServer({ uid }: Params): Genshin {
+export function getServer({ uid }: Params): Server {
   if (uid.startsWith("6")) {
-    return Genshin.America;
+    return Server.America;
   }
 
   if (uid.startsWith("7")) {
-    return Genshin.Europe;
+    return Server.Europe;
   }
 
   if (uid.startsWith("8") || uid.startsWith("18")) {
-    return Genshin.Asia;
+    return Server.Asia;
   }
 
   if (uid.startsWith("9")) {
-    return Genshin.TW_HK_MO;
+    return Server.TW_HK_MO;
   }
 
   throw new InvalidUID("taroh UID yang bener anying");
