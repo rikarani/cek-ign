75c4581a0cbb5ca34d9869eb6bd0d84874779f3a
rikarani <46773591+rikarani@users.noreply.github.com>
2024-10-09 20:24:55 +0700

    Refactor server port configuration in index.ts

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import { Elysia } from "elysia";
 
 import { mlbb, genshin } from "./plugins";
 
-const app = new Elysia().use(mlbb).use(genshin).listen(3000);
+const port = process.env.PORT || 6969;
+const app = new Elysia().use(mlbb).use(genshin).listen(port);
 
 console.log(`🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`);
