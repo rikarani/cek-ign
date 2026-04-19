3c62e22b04ef0806302cbbcb8e4b9cdfafc2e75c
Phasya Ananta <46773591+rikarani@users.noreply.github.com>
2026-04-20 01:29:22 +0700

    Add error message fields to custom errors

--- src/utils/errors.ts
+++ src/utils/errors.ts
@@ -21,14 +21,17 @@ abstract class CustomError extends Error {
 export class ExternalServerError extends CustomError {
   public code: string = "EXTERNAL_SERVER_ERROR";
   public status: number = StatusMap["Service Unavailable"];
+  public message: string = "Ada kesalahan saat melakukan request ke API Codashop";
 }
 
 export class AccountNotFoundError extends CustomError {
   public code: string = "ACCOUNT_NOT_FOUND";
   public status: number = StatusMap["Not Found"];
+  public message = "Akun tidak ditemukan";
 }
 
 export class InvalidUidError extends CustomError {
   public code: string = "INVALID_UID";
   public status: number = StatusMap["Unprocessable Content"];
+  public message: string = "ID / UID tidak valid";
 }
