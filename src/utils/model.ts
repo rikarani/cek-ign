c5ade402b04c577fbb1a2c291dfda89a103f6007
Phasya Ananta <46773591+rikarani@users.noreply.github.com>
2026-04-25 03:07:24 +0700

    Add Example type and enrich error responses

--- src/utils/model.ts
+++ src/utils/model.ts
@@ -1,53 +1,118 @@
 import { t } from "elysia";
 
-export const Error = {
-  notFound: t.Partial(
-    t.Object({
-      success: t.Literal(false, { description: "status" }),
-      error: t.Partial(
-        t.Object(
-          {
-            code: t.String({ description: "kode error" }),
-            message: t.String({ description: "pesan error" }),
-          },
-          { description: "detail error" },
-        ),
-      ),
-    }),
-    {
-      description: "akun yang dicari gak ketemu",
-      example: {
-        success: false,
-        error: {
-          code: "ACCOUNT_NOT_FOUND",
-          message: "Akun Tidak Ditemukan",
-        },
-      },
-    },
-  ),
+type Example = {
+  path: string;
+  message: string;
+  summary: string;
+};
 
-  serverError: t.Partial(
-    t.Object({
-      success: t.Literal(false, { description: "status" }),
-      error: t.Partial(
-        t.Object(
-          {
-            code: t.String({ description: "kode error" }),
-            message: t.String({ description: "pesan error" }),
+export const Error = {
+  badRequest: (example: Example[]) => {
+    return t.Partial(
+      t.Object(
+        {
+          success: t.Literal(false, { description: "status" }),
+          errors: t.Array(
+            t.Partial(
+              t.Object(
+                {
+                  path: t.String({ description: "tempat salahnye" }),
+                  message: t.String({ description: "pesan error singkat" }),
+                  summary: t.String({ description: "pesan error panjang lebar" }),
+                },
+                {
+                  description: "detail error",
+                },
+              ),
+            ),
+          ),
+        },
+        {
+          description: "salah format request",
+          example: {
+            success: false,
+            errors: example,
           },
-          { description: "detail error" },
-        ),
+        },
       ),
-    }),
-    {
-      description: "ada error di server",
-      example: {
-        success: false,
-        error: {
-          code: "EXTERNAL_SERVER_ERROR",
-          message: "Gagal melakukan request ke API Codashop",
+    );
+  },
+  notFound: t.Partial(
+    t.Object(
+      {
+        success: t.Literal(false, { description: "status" }),
+        error: t.Partial(
+          t.Object(
+            {
+              code: t.String({ description: "kode error" }),
+              message: t.String({ description: "pesan error" }),
+            },
+            { description: "detail error" },
+          ),
+        ),
+      },
+      {
+        description: "akun yang dicari gak ketemu",
+        example: {
+          success: false,
+          error: {
+            code: "ACCOUNT_NOT_FOUND",
+            message: "Akun Tidak Ditemukan",
+          },
         },
       },
-    },
+    ),
+  ),
+  invalidUid: t.Partial(
+    t.Object(
+      {
+        success: t.Literal(false, { description: "status" }),
+        error: t.Partial(
+          t.Object(
+            {
+              code: t.String({ description: "kode error" }),
+              message: t.String({ description: "pesan error" }),
+            },
+            { description: "detail error" },
+          ),
+        ),
+      },
+      {
+        description: "format ID / UID tidak valid",
+        example: {
+          success: false,
+          error: {
+            code: "INVALID_UID",
+            message: "masukkan UID yang benar",
+          },
+        },
+      },
+    ),
+  ),
+  serverError: t.Partial(
+    t.Object(
+      {
+        success: t.Literal(false, { description: "status" }),
+        error: t.Partial(
+          t.Object(
+            {
+              code: t.String({ description: "kode error" }),
+              message: t.String({ description: "pesan error" }),
+            },
+            { description: "detail error" },
+          ),
+        ),
+      },
+      {
+        description: "ada error di API Codashop",
+        example: {
+          success: false,
+          error: {
+            code: "EXTERNAL_SERVER_ERROR",
+            message: "Gagal melakukan request ke API Codashop",
+          },
+        },
+      },
+    ),
   ),
 };
