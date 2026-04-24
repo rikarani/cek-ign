d64f74b9776ae509ebbd7fb4a2ff11b7b0294329
Phasya Ananta <46773591+rikarani@users.noreply.github.com>
2026-04-25 03:07:57 +0700

    Replace custom badRequest with Error.badRequest

--- src/mlbb/model.ts
+++ src/mlbb/model.ts
@@ -1,15 +1,11 @@
 import { t } from "elysia";
 
+import { Error } from "../utils/model.js";
+
 export const Model = {
   query: t.Object({
-    id: t.String({
-      description: "ID akun yang mau dicek",
-      example: "471192087",
-    }),
-    zone: t.String({
-      description: "Zone akun yang mau dicek",
-      example: "2416",
-    }),
+    id: t.String({ description: "ID akun yang mau dicek", example: "471192087" }),
+    zone: t.String({ description: "Zone akun yang mau dicek", example: "2416" }),
   }),
   success: t.Partial(
     t.Object(
@@ -47,39 +43,16 @@ export const Model = {
       },
     ),
   ),
-  badRequest: t.Partial(
-    t.Object(
-      {
-        success: t.Literal(false, { description: "status" }),
-        errors: t.Array(
-          t.Partial(
-            t.Object({
-              path: t.String({ description: "tempat salahnye" }),
-              message: t.String({ description: "pesan error singkat" }),
-              summary: t.String({ description: "pesan error panjang lebar" }),
-            }),
-          ),
-          { description: "detail error" },
-        ),
-      },
-      {
-        description: "salah format request",
-        example: {
-          success: false,
-          errors: [
-            {
-              path: "/id",
-              message: "Expected string",
-              summary: "Expected property 'id' to be string but found: undefined",
-            },
-            {
-              path: "/zone",
-              message: "Expected string",
-              summary: "Expected property 'zone' to be string but found: undefined",
-            },
-          ],
-        },
-      },
-    ),
-  ),
+  badRequest: Error.badRequest([
+    {
+      path: "/id",
+      message: "Expected string",
+      summary: "Expected property 'id' to be string but found: undefined",
+    },
+    {
+      path: "/zone",
+      message: "Expected string",
+      summary: "Expected property 'zone' to be string but found: undefined",
+    },
+  ]),
 };
