0abd5cfbb6d60b10e22a8b520b6fc137f3e5911a
Phasya Ananta <46773591+rikarani@users.noreply.github.com>
2026-04-25 16:27:37 +0700

    Shorten MLBB detail description

--- src/mlbb/index.ts
+++ src/mlbb/index.ts
@@ -32,7 +32,6 @@ export default new Elysia().get("/mlbb", ({ query: { id, zone } }) => Mlbb.check
   },
   detail: {
     summary: "Mobile Legends: Bang-Bang",
-    description:
-      "Permainan video seluler bergenre multiplayer online battle arena yang dikembangkan dan diterbitkan oleh Moonton, anak perusahaan dari ByteDance",
+    description: "Game mobile bergenre multiplayer online battle arena yang dikembangkan dan diterbitkan oleh Moonton",
   },
 });
