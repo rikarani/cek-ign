73a95c3db41c0caf1241a70dd9b95404945ddd77
Phasya Ananta <46773591+rikarani@users.noreply.github.com>
2026-05-02 02:07:52 +0700

    Refactor Genshin model to use BaseModel

--- src/genshin/model.ts
+++ src/genshin/model.ts
@@ -1,48 +1,24 @@
 import { t } from "elysia";
 
-import { Error } from "../utils/model.js";
+import { Model as BaseModel, Error } from "../utils/model.js";
 
 export const Model = {
-  query: t.Object({
+  query: BaseModel.query({
     uid: t.String({ description: "uid akun yang mau dicari", example: "800029362" }),
   }),
-  success: t.Partial(
-    t.Object(
-      {
-        success: t.Literal(true, { description: "status" }),
-        data: t.Partial(
-          t.Object({
-            game: t.String({ description: "game yang direquest" }),
-            account: t.Partial(
-              t.Object(
-                {
-                  uid: t.String({ description: "UID akun yang dicari" }),
-                  server: t.String({ description: "Server akun" }),
-                  ign: t.String({ description: "in-game name" }),
-                },
-                {
-                  description: "detail akun",
-                },
-              ),
-            ),
-          }),
-        ),
+  success: BaseModel.success(
+    {
+      uid: t.String({ description: "UID akun yang dicari" }),
+      server: t.String({ description: "Server akun" }),
+    },
+    {
+      game: "Genshin Impact",
+      account: {
+        uid: "800029362",
+        server: "Asia",
+        ign: "A***a",
       },
-      {
-        description: "akun yang dicari ketemu",
-        example: {
-          success: true,
-          data: {
-            game: "Genshin Impact",
-            account: {
-              uid: "800029362",
-              server: "Asia",
-              ign: "A***a",
-            },
-          },
-        },
-      },
-    ),
+    },
   ),
   badRequest: Error.badRequest([
     {
