d69f84ce8286991c0abc00da4ecc7512fb1f223f
Phasya Ananta <46773591+rikarani@users.noreply.github.com>
2026-05-07 01:35:20 +0700

    Add 8 Ball Pool API Route

---
+++ src/8-ball-pool/service.ts
@@ -0,0 +1,35 @@
+import { UnwrapSchema } from "elysia";
+
+import { Model } from "./model.js";
+import { Fetcher } from "../utils/fetcher.js";
+import { AccountNotFoundError, ExternalServerError } from "../utils/errors.js";
+
+import { Response } from "../types/helper.js";
+import { CodashopResponse } from "../types/shared.js";
+
+type Success = UnwrapSchema<typeof Model.success>;
+
+export abstract class EightBallPool {
+  public static async check({ id }: UnwrapSchema<typeof Model.query>): Promise<Response<Success>> {
+    const data = await Fetcher.codashop({
+      vpp: { id: "205678", price: "140000", vp: "0" },
+      user: { userId: id, zoneId: "" },
+      voucherTypeName: "EIGHT_BALL_POOL",
+    });
+
+    if (data.errorCode == "-100") {
+      throw new AccountNotFoundError();
+    }
+
+    return {
+      success: true,
+      data: {
+        game: data.confirmationFields.productName,
+        account: {
+          id,
+          ign: decodeURIComponent(data.confirmationFields.username).replace(/\+/g, " "),
+        },
+      },
+    };
+  }
+}
