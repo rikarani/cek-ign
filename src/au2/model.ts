181ca243e4ac77e8f73f9bd5393bcedd95be56d1
Phasya Ananta <46773591+rikarani@users.noreply.github.com>
2026-05-13 23:57:36 +0700

    add game

---
+++ src/au2/model.ts
@@ -0,0 +1,28 @@
+import { t } from "elysia";
+
+import { Model as BaseModel, Error } from "../utils/model.js";
+
+export const Model = {
+  query: BaseModel.query({
+    id: t.String({ pattern: "^[0-9]+$", description: "ID akun", example: "662311" }),
+  }),
+  success: BaseModel.success(
+    {
+      id: t.String({ description: "ID akun yang dicari" }),
+    },
+    {
+      game: "AU2",
+      account: {
+        id: "662311",
+        ign: "Juan Bour",
+      },
+    },
+  ),
+  badRequest: Error.badRequest([
+    {
+      path: "/id",
+      message: "Expected string",
+      summary: "Expected property 'id' to be string but found: undefined",
+    },
+  ]),
+};
