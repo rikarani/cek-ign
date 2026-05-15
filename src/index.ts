cdf7997e89dcf25e56b6d1cc007ecee13d16da69
Phasya Ananta <46773591+rikarani@users.noreply.github.com>
2026-05-15 23:47:29 +0700

    Add Azur Lane API integration

--- src/index.ts
+++ src/index.ts
@@ -9,11 +9,12 @@ import aov from "./arena-of-valor/index.js";
 import asphalt9Legends from "./asphalt-9-legends/index.js";
 import au2 from "./au2/index.js";
 import autoChess from "./auto-chess/index.js";
+import azurLane from "./azur-lane/index.js";
 
 const app = new Elysia()
   .use(cors(config.cors))
   .use(openapi(config.openapi))
-  .group("/api", (app) => app.use(eightBallPool).use(aov).use(asphalt9Legends).use(au2).use(autoChess))
+  .group("/api", (app) => app.use(eightBallPool).use(aov).use(asphalt9Legends).use(au2).use(autoChess).use(azurLane))
   .get("/", ({ redirect }) => redirect("/openapi"));
 
 if (process.env.NODE_ENV !== "production") {
