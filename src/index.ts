afe2bad4f55ec0b3bc37c9217159788e13abc60b
Phasya Ananta <46773591+rikarani@users.noreply.github.com>
2026-05-15 15:40:07 +0700

    add Auto Chess

--- src/index.ts
+++ src/index.ts
@@ -8,11 +8,12 @@ import eightBallPool from "./8-ball-pool/index.js";
 import aov from "./arena-of-valor/index.js";
 import asphalt9Legends from "./asphalt-9-legends/index.js";
 import au2 from "./au2/index.js";
+import autoChess from "./auto-chess/index.js";
 
 const app = new Elysia()
   .use(cors(config.cors))
   .use(openapi(config.openapi))
-  .group("/api", (app) => app.use(eightBallPool).use(aov).use(asphalt9Legends).use(au2))
+  .group("/api", (app) => app.use(eightBallPool).use(aov).use(asphalt9Legends).use(au2).use(autoChess))
   .get("/", ({ redirect }) => redirect("/openapi"));
 
 if (process.env.NODE_ENV !== "production") {
