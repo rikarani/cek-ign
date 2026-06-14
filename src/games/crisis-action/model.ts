f9f825fad3133496f7079b8862967df05229307a
Phasya Ananta <46773591+rikarani@users.noreply.github.com>
2026-06-15 03:14:37 +0700

    biome

--- src/games/crisis-action/model.ts
+++ src/games/crisis-action/model.ts
@@ -1,36 +1,40 @@
-import { t } from "elysia";
+import { t } from 'elysia';
 
-import { servers, type Servers } from "./server";
+import { type Servers, servers } from './server';
 
-import { Model as BaseModel, Error } from "../../utils/model";
+import { Model as BaseModel, ResponseError } from '../../utils/model';
 
 export const Model = {
   query: BaseModel.query({
-    id: t.String({ pattern: "^[0-9]+$", description: "ID akun yang mau dicek", example: "483395457" }),
+    id: t.String({
+      pattern: '^[0-9]+$',
+      description: 'ID akun yang mau dicek',
+      example: '483395457',
+    }),
     server: t.UnionEnum(Object.keys(servers) as [Servers, ...Servers[]], {
-      description: "Server akun yang mau dicek",
+      description: 'Server akun yang mau dicek',
     }),
   }),
   success: BaseModel.success(
     {
-      id: t.String({ description: "ID akun yang dicari" }),
+      id: t.String({ description: 'ID akun yang dicari' }),
       server: t.UnionEnum(Object.keys(servers) as [Servers, ...Servers[]], {
-        description: "Server akun yang dicari",
+        description: 'Server akun yang dicari',
       }),
     },
     {
-      game: "Crisis Action",
+      game: 'Crisis Action',
       account: {
-        id: "483395457",
-        server: "Alliance",
-        ign: "kontorru",
+        id: '483395457',
+        server: 'Alliance',
+        ign: 'kontorru',
       },
     },
   ),
-  badRequest: Error.badRequest([
+  badRequest: ResponseError.badRequest([
     {
-      path: "/id",
-      message: "Expected string",
+      path: '/id',
+      message: 'Expected string',
       summary: "Expected property 'id' to be string but found: undefined",
     },
   ]),
--- src/games/crisis-action/service.ts
+++ src/games/crisis-action/service.ts
@@ -1,22 +1,23 @@
-import { UnwrapSchema } from "elysia";
+import type { UnwrapSchema } from 'elysia';
 
-import { Model } from "./model";
-import { servers } from "./server";
+import type { Model } from './model';
 
-import { Fetcher } from "../../utils/fetcher";
-import { AccountNotFoundError } from "../../utils/errors";
+import type { Response } from '../../types/helper';
 
-import { Response } from "../../types/helper";
+import { servers } from './server';
+
+import { Fetcher } from '../../utils/fetcher';
+import { AccountNotFoundError } from '../../utils/errors';
 
 export const CrisisAction = {
   async check({ id, server }: UnwrapSchema<typeof Model.query>): Promise<Response<UnwrapSchema<typeof Model.success>>> {
     const data = await Fetcher.codashop({
-      vpp: { id: "3745", price: "300000", vp: "0" },
+      vpp: { id: '3745', price: '300000', vp: '0' },
       user: { userId: id, zoneId: servers[server] },
-      voucherTypeName: "HEROGAMES",
+      voucherTypeName: 'HEROGAMES',
     });
 
-    if (data.errorCode === "-100" || data.errorCode === "-102") {
+    if (data.errorCode === '-100' || data.errorCode === '-102') {
       throw new AccountNotFoundError();
     }
 
@@ -27,7 +28,7 @@ export const CrisisAction = {
         account: {
           id,
           server,
-          ign: decodeURIComponent(data.confirmationFields.username).replace(/\+/g, " "),
+          ign: decodeURIComponent(data.confirmationFields.username).replace(/\+/g, ' '),
         },
       },
     };
