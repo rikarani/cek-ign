3172333fec3de3c37b6cdaeda13d87bb2ade721e
Phasya Ananta <46773591+rikarani@users.noreply.github.com>
2026-06-15 03:04:31 +0700

    biome

--- src/games/arena-of-valor/model.ts
+++ src/games/arena-of-valor/model.ts
@@ -1,27 +1,31 @@
-import { t } from "elysia";
+import { t } from 'elysia';
 
-import { Model as BaseModel, Error } from "../../utils/model";
+import { Model as BaseModel, ResponseError } from '../../utils/model';
 
 export const Model = {
   query: BaseModel.query({
-    id: t.String({ pattern: "^[0-9]+$", description: "ID akun yang mau dicek", example: "888347346994333" }),
+    id: t.String({
+      pattern: '^[0-9]+$',
+      description: 'ID akun yang mau dicek',
+      example: '888347346994333',
+    }),
   }),
   success: BaseModel.success(
     {
-      id: t.String({ description: "ID akun yang dicari" }),
+      id: t.String({ description: 'ID akun yang dicari' }),
     },
     {
-      game: "Arena of Valor",
+      game: 'Arena of Valor',
       account: {
-        id: "888347346994333",
-        ign: "BangRams",
+        id: '888347346994333',
+        ign: 'BangRams',
       },
     },
   ),
-  badRequest: Error.badRequest([
+  badRequest: ResponseError.badRequest([
     {
-      path: "/id",
-      message: "Expected string",
+      path: '/id',
+      message: 'Expected string',
       summary: "Expected property 'id' to be string but found: undefined",
     },
   ]),
--- src/games/arena-of-valor/service.ts
+++ src/games/arena-of-valor/service.ts
@@ -1,18 +1,18 @@
-import { UnwrapSchema } from "elysia";
+import type { UnwrapSchema } from 'elysia';
 
-import { Model } from "./model";
+import type { Model } from './model';
 
-import { Fetcher } from "../../utils/fetcher";
-import { AccountNotFoundError } from "../../utils/errors";
+import type { Response } from '../../types/helper';
 
-import { Response } from "../../types/helper";
+import { Fetcher } from '../../utils/fetcher';
+import { AccountNotFoundError } from '../../utils/errors';
 
 export const ArenaOfValor = {
   async check({ id }: UnwrapSchema<typeof Model.query>): Promise<Response<UnwrapSchema<typeof Model.success>>> {
     const data = await Fetcher.codashop({
-      vpp: { id: "8003", price: "300000", vp: "0" },
-      user: { userId: id, zoneId: "" },
-      voucherTypeName: "AOV",
+      vpp: { id: '8003', price: '300000', vp: '0' },
+      user: { userId: id, zoneId: '' },
+      voucherTypeName: 'AOV',
     });
 
     if (data.errorCode === 12) {
@@ -22,10 +22,10 @@ export const ArenaOfValor = {
     return {
       success: true,
       data: {
-        game: "Arena of Valor",
+        game: data.confirmationFields.productName,
         account: {
           id,
-          ign: decodeURIComponent(data.confirmationFields.roles[0].role).replace(/\+/g, " "),
+          ign: decodeURIComponent(data.confirmationFields.roles[0].role).replace(/\+/g, ' '),
         },
       },
     };
