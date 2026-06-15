203b6c76e8e5fe63ecf6b9e6a4f2aefe3c2bf845
Phasya Ananta <46773591+rikarani@users.noreply.github.com>
2026-06-15 17:46:46 +0700

    biome

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,24 @@
-import { Elysia } from "elysia";
-import { cors } from "@elysiajs/cors";
-import { openapi } from "@elysiajs/openapi";
+import { Elysia } from 'elysia';
+import { cors } from '@elysiajs/cors';
+import { openapi } from '@elysiajs/openapi';
 
-import { config } from "./utils/config";
+import { config } from './utils/config';
 
-import eightBallPool from "./games/8-ball-pool/index";
-import aov from "./games/arena-of-valor/index";
-import au2 from "./games/au2/index";
-import autoChess from "./games/auto-chess/index";
-import azurLane from "./games/azur-lane/index";
-import callOfDutyMobile from "./games/call-of-duty-mobile/index";
-import captainTsubasa from "./games/captain-tsubasa-dream-team/index";
-import crisisAction from "./games/crisis-action";
-import dragonCity from "./games/dragon-city";
+import au2 from './games/au2';
+import aov from './games/arena-of-valor';
+import azurLane from './games/azur-lane';
+import autoChess from './games/auto-chess';
+import dragonCity from './games/dragon-city';
+import dragonRaja from './games/dragon-raja';
+import eightBallPool from './games/8-ball-pool';
+import crisisAction from './games/crisis-action';
+import callOfDutyMobile from './games/call-of-duty-mobile';
+import captainTsubasa from './games/captain-tsubasa-dream-team';
 
 const app = new Elysia()
   .use(cors(config.cors))
   .use(openapi(config.openapi))
-  .group("/api", (app) =>
+  .group('/api', (app) =>
     app
       .use(eightBallPool)
       .use(aov)
@@ -27,11 +28,12 @@ const app = new Elysia()
       .use(callOfDutyMobile)
       .use(captainTsubasa)
       .use(crisisAction)
-      .use(dragonCity),
+      .use(dragonCity)
+      .use(dragonRaja),
   )
-  .get("/", ({ redirect }) => redirect("/openapi"));
+  .get('/', ({ redirect }) => redirect('/openapi'));
 
-if (process.env.NODE_ENV !== "production") {
+if (process.env.NODE_ENV !== 'production') {
   app.listen(config.port);
   console.log(`Server running at ${app.server?.hostname}:${app.server?.port}`);
 }
