4cb3ff930c52b7231c72717c06583ed6f18d53c7
Phasya Ananta <46773591+rikarani@users.noreply.github.com>
2026-06-21 17:57:56 +0700

    Add Onmyoji Arena endpoint and service

---
+++ src/games/onmyoji-arena/model.ts
@@ -0,0 +1,32 @@
+import { t } from 'elysia';
+
+import { Model as BaseModel, ResponseError } from '../../utils/model';
+
+export const Model = {
+  query: BaseModel.query({
+    id: t.String({
+      pattern: '^[0-9]+$',
+      description: 'ID akun yang mau dicek',
+      example: '3022022128',
+    }),
+  }),
+  success: BaseModel.success(
+    {
+      id: t.String({ description: 'ID akun yang dicari' }),
+    },
+    {
+      game: 'Onmyoji Arena',
+      account: {
+        id: '3022022128',
+        ign: 'ギュっ子',
+      },
+    },
+  ),
+  badRequest: ResponseError.badRequest([
+    {
+      path: '/id',
+      message: 'Expected string',
+      summary: "Expected property 'id' to be string but found: undefined",
+    },
+  ]),
+};
