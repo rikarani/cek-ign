fba560aaf3be36a5875ccc27e1a7f27daaf43083
Phasya Ananta <46773591+rikarani@users.noreply.github.com>
2026-06-21 17:59:06 +0700

    Update game imports and routes and remove dev log

--- src/index.ts
+++ src/index.ts
@@ -4,44 +4,43 @@ import { openapi } from '@elysiajs/openapi';
 
 import { config } from './utils/config';
 
-import au2 from './games/au2';
 import aov from './games/arena-of-valor';
 import azurLane from './games/azur-lane';
-import autoChess from './games/auto-chess';
-import dragonCity from './games/dragon-city';
-import dragonRaja from './games/dragon-raja';
-import eightBallPool from './games/8-ball-pool';
-import crisisAction from './games/crisis-action';
 import callOfDutyMobile from './games/call-of-duty-mobile';
-import captainTsubasa from './games/captain-tsubasa-dream-team';
-import footballMaster2 from './games/football-master-2';
 import freeFire from './games/free-fire';
 import genshinImpact from './games/genshin-impact';
+import honkaiImpact3 from './games/honkai-impact-3';
+import honkaiStarRail from './games/honkai-star-rail';
+import loveAndDeepspace from './games/love-and-deepspace';
+import magicChessGoGo from './games/magic-chess-go-go';
+import mobileLegends from './games/mobile-legends';
+import onmyojiArena from './games/onmyoji-arena';
+import stellaSora from './games/stella-sora';
+import zenlessZoneZero from './games/zenless-zone-zero';
 
 const app = new Elysia()
   .use(cors(config.cors))
   .use(openapi(config.openapi))
   .group('/api', (app) =>
     app
-      .use(eightBallPool)
       .use(aov)
-      .use(au2)
-      .use(autoChess)
       .use(azurLane)
       .use(callOfDutyMobile)
-      .use(captainTsubasa)
-      .use(crisisAction)
-      .use(dragonCity)
-      .use(dragonRaja)
-      .use(footballMaster2)
       .use(freeFire)
-      .use(genshinImpact),
+      .use(genshinImpact)
+      .use(honkaiImpact3)
+      .use(honkaiStarRail)
+      .use(loveAndDeepspace)
+      .use(magicChessGoGo)
+      .use(mobileLegends)
+      .use(onmyojiArena)
+      .use(stellaSora)
+      .use(zenlessZoneZero),
   )
   .get('/', ({ redirect }) => redirect('/openapi'));
 
 if (process.env.NODE_ENV !== 'production') {
   app.listen(config.port);
-  console.log(`Server running at ${app.server?.hostname}:${app.server?.port}`);
 }
 
 export default app;
